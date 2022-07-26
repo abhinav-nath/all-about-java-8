3e23bff734e5754df825ea1e21eb5a734c3c7c67
Abhinav Nath <abhinavnath@ymail.com>
2022-07-27 02:35:05 +0530

    Fix Sonar issue

--- src/com/codecafe/java8/streams/usecases/ReadFromFile.java
+++ src/com/codecafe/java8/streams/usecases/ReadFromFile.java
@@ -3,16 +3,22 @@ package com.codecafe.java8.streams.usecases;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 // Read a file and count the occurrence of a word in it
 public class ReadFromFile {
 
   public static void main(String[] args) throws IOException {
-    String path = "C:\\Users\\Abhinav\\git\\java-8\\src\\com\\codecafe\\java8\\streams\\usecases\\hosts";
+    String path = "src/com/codecafe/java8/streams/usecases/hosts";
     String searchWord = "localhost";
-    System.out.printf("occurrence of word [%s] = %d", searchWord, Files.lines(Paths.get(path))
-                                                                       .filter(line -> line.contains(searchWord))
-                                                                       .count());
+    long count = 0;
+
+    try (Stream<String> lines = Files.lines(Paths.get(path))) {
+      count = lines.filter(line -> line.contains(searchWord))
+                   .count();
+    }
+
+    System.out.printf("occurrence of word [%s] = %d", searchWord, count);
   }
 
 }
